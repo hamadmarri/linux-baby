ec466afbb67b1ae2a6247912d0e411b315c092b1
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-10-28 13:57:55 +0300

    copy prio_changed_fair and switched_to_fair from cfs

--- kernel/sched/bs.h
+++ kernel/sched/bs.h
@@ -125,7 +125,25 @@ account_entity_dequeue(struct cfs_rq *cfs_rq, struct sched_entity *se)
 
 
 static void
-prio_changed_fair(struct rq *rq, struct task_struct *p, int oldprio) {}
+prio_changed_fair(struct rq *rq, struct task_struct *p, int oldprio)
+{
+	if (!task_on_rq_queued(p))
+		return;
+
+	if (rq->cfs.nr_running == 1)
+		return;
+
+	/*
+	 * Reschedule if we are currently running on this runqueue and
+	 * our priority decreased, or if we are not currently running on
+	 * this runqueue and our priority is higher than the current's
+	 */
+	if (task_current(rq, p)) {
+		if (p->prio > oldprio)
+			resched_curr(rq);
+	} else
+		check_preempt_curr(rq, p, 0);
+}
 
 static void switched_from_fair(struct rq *rq, struct task_struct *p) {}
 
@@ -137,7 +155,10 @@ static void switched_to_fair(struct rq *rq, struct task_struct *p)
 		 * kick off the schedule if running, otherwise just see
 		 * if we can still preempt the current task.
 		 */
-		resched_curr(rq);
+		if (task_current(rq, p))
+			resched_curr(rq);
+		else
+			check_preempt_curr(rq, p, 0);
 	}
 }
 
